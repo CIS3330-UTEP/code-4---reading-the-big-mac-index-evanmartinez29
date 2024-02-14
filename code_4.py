744fb0158978884fa5f1f9ac47ceefef40e642c4
evanmartinez29 <157407999+evanmartinez29@users.noreply.github.com>
2024-02-14 02:55:33 -0700

    update code_4 vs 1.py
    created function that got the price of bog mac by country using .query()

--- code_4.py
+++ code_4.py
@@ -1,12 +1,22 @@
 import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
+df = pd.read_csv(big_mac_file)
 
 def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
+    year = "2012"
+    country_code = "ARG"
+    query_text = f"date == @year"
+    df_year = df.query(query_text)
+    print(round(df_year['dollar_price'].mean(), 2))
+    return df_year
 
 def get_big_mac_price_by_country(country_code):
-    pass # Remove this line and code your function
+    country_code = "ARG"
+    query_text = f"iso_a3 == @country_code"
+    df_country = df.query(query_text)
+    print(round(df_country['dollar_price'].mean(), 2))
+    return df_country
 
 def get_the_cheapest_big_mac_price_by_year(year):
     pass # Remove this line and code your function
@@ -15,4 +25,7 @@ def get_the_most_expensive_big_mac_price_by_year(year):
     pass # Remove this line and code your function
 
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
+    mac_price_year = get_big_mac_price_by_year(year="2012", country_code="ARG")
+    
+
+    mac_price_coutry = get_big_mac_price_by_country("ARG")
---
+++ pandas_sandbox.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+# list_teams = ['49ers', 'Cheifs', 'Cowboys', 'Steelers']
+
+# print(type(list_teams))
+
+# print(list_teams)
+
+# series_teams = pd.Series(list_teams)
+
+# print(series_teams)
+# print(type(series_teams))
+# print(series_teams.index)
+
+
+# students = {'Hawai':'Isabella', 'Ohia':"David", 'Iowa':'Justin', 'Colorado':'Robert'}
+
+# print(type(students))
+# print(students)
+
+# students_series = pd.Series(index=students.keys(), data=students.values())
+
+# print(type(students_series))
+# print(students_series)
+# print(students_series.index)
+
+df = pd.read_csv('big-mac-full-index.csv')
+
+# print(df)
+
+# print(type(df))
+# print(df.columns)
+# print(type(df.columns))
+
+# print(df['iso_a3'])
+
+
+# print((type(df['iso_a3']))
+
+country_code = '2008-06-01'
+query_text = f"date == @country_code" #can also use query_text = f"iso_a3 == 'USA'"
+df_arg = df.query(query_text)
+
+print(round(df_arg['dollar_price'].mean(), 2))
+print(df_arg['dollar_price'].min())
+# print(df_arg['dollar_price'].max())
+
+# idx_dollar_price = df_arg['dollar_price'].idxmax()
+# print(idx_dollar_price)
+
+# arg_series = df_arg.loc[idx_dollar_price]   
+# print(arg_series)
+# print(type(arg_series))
+
+# print(df_arg['local_price'].median())
+
+
+
+
